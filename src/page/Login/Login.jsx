c0eba2253473821fb662cdfddf00b99fdb5727ae
yangting11 <2039048627@qq.com>
2018-11-03 22:15:29 +0800

    环境

---
+++ src/page/Login/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Login.less'
+class Login extends React.Component{
+    render(){
+        return(
+            <div className="loginPage">
+
+            </div>
+        )
+    }
+}
+export default Login
