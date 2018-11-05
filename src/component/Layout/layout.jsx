02e0ed4c782b54ce9951cd178f483efd9aef9c6d
杨婷 <2039048627@qq.com>
2018-11-05 16:59:49 +0800

    new layout

--- src/component/Layout/layout.jsx
+++ src/component/Layout/layout.jsx
@@ -1,23 +1,67 @@
 import React from 'react'
-import { Layout } from 'antd';
+import { Layout, Menu, Icon  } from 'antd';
+import { Link } from 'react-router-dom'
 import './layout.css'
 const { Header, Footer, Sider, Content } = Layout;
 
 class LayoutItem extends React.Component{
+    state = {
+        collapsed: false,
+    };
+
+    toggle = () => {
+        this.setState({
+            collapsed: !this.state.collapsed,
+        });
+    }
     render(){
         return(
-            <div>
+            <Layout>
+                <Header style={{ background: '#fff', padding: 0 }}>
+                    Header
+                </Header>
                 <Layout>
-                    <Header className="header">Header</Header>
+                    <Sider
+                        trigger={null}
+                        collapsible
+                        collapsed={this.state.collapsed}
+                    >
+                        <div className="icon">
+                            <Icon
+                                className="trigger"
+                                type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
+                                onClick={this.toggle}
+                            />
+                        </div>
+                        <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+                            <Menu.Item key="1">
+                                <Link to="/home">
+                                    <Icon type="user" />
+                                    <span>首页</span>
+                                </Link>
+                            </Menu.Item>
+                            <Menu.Item key="2">
+                                <Link to="/home">
+                                    <Icon type="video-camera" />
+                                    <span>第二个页面</span>
+                                </Link>
+                            </Menu.Item>
+                            <Menu.Item key="3">
+                                <Link to="/home">
+                                    <Icon type="upload" />
+                                    <span>第三个页面</span>
+                                </Link>
+                            </Menu.Item>
+                        </Menu>
+                    </Sider>
                     <Layout>
-                        <Sider className="sider">Sider</Sider>
-                        <Content className="content">
-                            {this.props.children}
+                        <Content style={{ margin: '24px 16px', padding: 24, background: '#fff', minHeight: 280 }}>
+                            Content
                         </Content>
                     </Layout>
-                    <Footer className="footer">11111111</Footer>
                 </Layout>
-            </div>
+            </Layout>
+
         )
     }
 }
